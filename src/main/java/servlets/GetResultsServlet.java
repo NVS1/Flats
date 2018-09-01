57094bf413add74323e15886e4b2cd27b57fde5b
Ivan <nazarchukvan9@gmail.com>
2018-09-01 13:54:01 +0300

    flats ready

--- src/main/java/servlets/GetResultsServlet.java
+++ src/main/java/servlets/GetResultsServlet.java
@@ -1,7 +1,7 @@
-package Servlets;
+package servlets;
 
-import Utils.Filter;
-import Utils.FilterPredicates;
+import utils.Filter;
+import utils.FilterPredicates;
 import dao.ConnectionFactory;
 import dao.FlatDAO;
 import dao.FlatDAOimpl;
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.net.Inet4Address;
 import java.sql.Connection;
 import java.util.List;
 
